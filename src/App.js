cd7b7b4ac4d5a2db7f9eb7a064f1c7afa7fd8739
KIMNEMO <madamnurse43@gmail.com>
2024-09-02 14:58:51 +0900

    first commit react project
    first commit react project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import{
+  createBrowserRouter,RouterProvider
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element : <Home/>
+    },
+    {
+      path: "/movie/:id",
+      element : <Detail/>
+    },
+  ]);
+  return <RouterProvider router={router}/>;
+}
+
+export default App;
